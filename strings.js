3eb8f100d3b9744f6cca011c74d6b328e51eb0b3
krieger27 <krieger@vols.utk.edu>
2017-12-22 12:21:27 -0600

    commit before trying js letters only function

--- strings.js
+++ strings.js
@@ -1,4 +1,4 @@
-let userInput= document.getElementById("userInput");
+let userText= document.getElementById("userInput");
 let output= document.getElementById("output");
 let theBtn = document.getElementById("btn");
 
@@ -6,17 +6,35 @@ function toTheDom (value){
     output.innerHTML = value;
 }
 
-function reversal() {
-    
+theBtn.addEventListener("click", reversal);
+
+function reversal (userInput) {
+    let captureText = userText.value;
+    let outputValue = "";
+    let stringToArray = captureText.split("");
+    let reverse = stringToArray.reverse().join("");
+    outputValue += `reverse it: "${reverse}".` + "<br>";
+    toTheDom(outputValue);
     }
     
-function alphabits() {
+function alphabits(userInput) {
+    let captureText = userText.value;
+    let outputValue = "";
+    let stringToArray = captureText.split("");
+    let alphabetical = stringToArray.sort().join("");
+    outputValue += `alphabetize it: "${alphabetical}".` + "<br>";
+    toTheDom(outputValue);
+}
     
-    }
-    
-function palindrome() {
-    
-    }
+function palindrome(userInput) {
+    let captureText = userText.value;
+    let outputValue = "";
+    let stringToArray = captureText.split("");
+    let reverse = stringToArray.reverse().join("");
+    if (reverse === captureText)
+    outputValue += `reverse it: "${reverse}".`;
+    toTheDom(outputValue);
+}
     
  var testString = "";
     reversal(testString);
