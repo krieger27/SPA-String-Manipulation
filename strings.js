f2e322fed90e1b16baa5e4df07c0384abf186ddc
krieger27 <krieger@vols.utk.edu>
2018-01-02 08:44:31 -0600

    works but needs function for limiting numbers since Aa-Zz not working

--- strings.js
+++ strings.js
@@ -8,6 +8,31 @@ function toTheDom (value){
 
 theBtn.addEventListener("click", reversal);
 
+
+//Lets hope this works//
+var alphaOnly = /[A-Za-z]/g;
+
+function restrictCharacters(myfield, e, restrictionType) {
+if (!e) var e = window.event
+if (e.keyCode) code = e.keyCode;
+else if (e.which) code = e.which;
+var character = String.fromCharCode(code);
+
+// if they pressed esc... remove focus from field...
+if (code==27) { this.blur(); return false; }
+
+// ignore if they are press other keys
+// strange because code: 39 is the down key AND ' key...
+// and DEL also equals .
+if (!e.ctrlKey && code!=9 && code!=8 && code!=36 && code!=37 && code!=38 && (code!=39 || (code==39 && character=="'")) && code!=40) {
+if (character.match(restrictionType)) {
+return true;
+} else {
+return false;
+}
+}
+}
+
 function reversal (userInput) {
     let captureText = userText.value;
     let outputValue = "";
